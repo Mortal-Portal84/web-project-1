7d493f34a9b41c96dc9d163b40201f6d48a0b564
Mortal-Portal84 <gavnorock84@hotmail.com>
2021-01-08 08:31:51 +0300

    prepare work with slider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 //  JavaScript
-import example from './js/example'
+import example from './js/example';
 
 //  HTML
-import './index.html'
+import './index.html';
+
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
 
 // Styles
-import './styles/index.scss'
+import './styles/index.scss';
 
+Swiper.use([Navigation, Pagination]);
 
-example()
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  
+example();
