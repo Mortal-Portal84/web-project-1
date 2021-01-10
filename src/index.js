f6c2e20514cb27c7db7332ad92e01af468b9e79f
Mortal-Portal84 <gavnorock84@hotmail.com>
2021-01-10 22:41:27 +0300

    slider ready, position was fixed in many places

--- src/index.js
+++ src/index.js
@@ -20,7 +20,12 @@ const swiper = new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
-  });
+    },
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,            
+    },
+  }); 
   
 example();
